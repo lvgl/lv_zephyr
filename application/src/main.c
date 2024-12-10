a610f283d00491ce3ec47a3fc9281a1a30d6e633
Felipe Neves <ryukokki.felipe@gmail.com>
2024-12-10 16:04:26 -0300

    m5stack: added manifest to m5stack core2
    Signed-off-by: Felipe Neves <ryukokki.felipe@gmail.com>

---
+++ application/src/main.c
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2024 LVGL <felipe@lvgl.io>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/display.h>
+#include <lvgl.h>
+#include <lvgl_mem.h>
+#include <lv_demos.h>
+#include <stdio.h>
+
+#define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(app);
+
+int main(void)
+{
+	const struct device *display_dev;
+
+	display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+	if (!device_is_ready(display_dev)) {
+		LOG_ERR("Device not ready, aborting test");
+		return 0;
+	}
+
+	/* Place your UI demo here, or try other demos */
+	lv_demo_widgets();
+
+	lv_timer_handler();
+	display_blanking_off(display_dev);
+#ifdef CONFIG_LV_Z_MEM_POOL_SYS_HEAP
+	lvgl_print_heap_info(false);
+#else
+	printf("lvgl in malloc mode\n");
+#endif
+	while (1) {
+		uint32_t sleep_ms = lv_timer_handler();
+		k_msleep(MIN(sleep_ms, INT32_MAX));
+	}
+
+	return 0;
+}
